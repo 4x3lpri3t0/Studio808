e3db9e52f0e498a1033cc7ca4f31dc50140fbccd
Axel Prieto <prietoaxel@gmail.com>
2021-06-27 13:50:50 +0200

    Add OperationStatus enum

---
+++ Studio808.BusinessLogic/Enums/OperationStatus.cs
@@ -0,0 +1,11 @@
+﻿namespace Studio808.BusinessLogic.Enums
+{
+    public enum OperationStatus
+    {
+        UnknownError = -1,
+        Success = 0,
+        AlreadyExists = 1,
+        NotFound = 2,
+        ExpectedStateMismatch = 3, // Concurrency issue due to race condition.
+    }
+}
