20890b2955eff402fab06e127c932b937e8ffb51
Axel Prieto <prietoaxel@gmail.com>
2021-06-27 19:32:30 +0200

    Use const for MediaType

--- Api.Tests/Helpers/TestHelper.cs
+++ Api.Tests/Helpers/TestHelper.cs
@@ -9,6 +9,8 @@ namespace Tests.Helpers
 {
     public class TestHelper
     {
+        private const string MediaType = "application/json";
+
         /// <summary>
         /// Post an object and check the response headers.
         /// </summary>
@@ -16,7 +18,7 @@ namespace Tests.Helpers
             HttpClient client, string url, object content, HttpStatusCode expectedCode)
         {
             string jsonAsString = JsonSerializer.Serialize(content);
-            var stringContent = new StringContent(jsonAsString, Encoding.UTF8, "application/json");
+            var stringContent = new StringContent(jsonAsString, Encoding.UTF8, MediaType);
             var response = await client.PostAsync(url, stringContent);
 
             Assert.Equal(expectedCode, response.StatusCode);
@@ -34,7 +36,7 @@ namespace Tests.Helpers
             HttpClient client, string url, object content, HttpStatusCode expectedCode)
         {
             string jsonAsString = JsonSerializer.Serialize(content);
-            var stringContent = new StringContent(jsonAsString, Encoding.UTF8, "application/json");
+            var stringContent = new StringContent(jsonAsString, Encoding.UTF8, MediaType);
             var response = await client.PutAsync(url, stringContent);
 
             Assert.Equal(expectedCode, response.StatusCode);
@@ -43,7 +45,7 @@ namespace Tests.Helpers
 
         private static void ValidateMediaType(HttpResponseMessage response)
         {
-            Assert.Equal("application/json", response.Content.Headers.ContentType.MediaType);
+            Assert.Equal(MediaType, response.Content.Headers.ContentType.MediaType);
         }
     }
 }
